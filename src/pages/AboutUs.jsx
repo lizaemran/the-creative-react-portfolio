885985eb7f3909c91108b787c91c17bab3f5e1bc
lizaemran <lizaemran56@gmail.com>
2021-07-17 13:09:36 +0500

    3. Services Section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import AboutSection from '../components/AboutSection'
+import ServicesSection from '../components/ServicesSection'
 const About = () => {
     return (
+    <> //fraction 
         <AboutSection/>
-    )
+        <ServicesSection />
+    </>
+    );
 }
 
 export default About

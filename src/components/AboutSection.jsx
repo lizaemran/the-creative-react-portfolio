444616dfe84bc416170aeb2dd91c2aff00a66c3e
lizaemran <lizaemran56@gmail.com>
2021-07-18 10:46:06 +0500

    11. framer motion

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -2,21 +2,30 @@ import React from 'react';
 import home1 from '../img/home1.png';
 import styled from 'styled-components';
 import { About, Description, Image, Hide } from '../styles';
+import {motion} from "framer-motion";
 const AboutSection = () => {
+    const titleAnim = {
+        hidden: {opacity: 0},
+        show: {opacity: 1, transition: {duration: 2}},
+    }
+    const container = {
+        hidden : {x: 100},
+        show: {x: 0, transition: {duration: 0.75, ease: 'easeOut' , staggerChildren: 1, when:"beforeChildren"}},
+    }
     return (
         <About>
             <Description>
-                <div className="title">
+                <motion.div variants={container} initial="hidden" animate="show" className="title">
                     <Hide>
-                        <h2>We work to make</h2>
+                        <motion.h2 variants={titleAnim}>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>your <span>dreams</span> come</h2>
+                        <motion.h2 variants={titleAnim}>your <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true.</h2>
+                        <motion.h2 variants={titleAnim}h2>true.</motion.h2>
                     </Hide>
-                </div>
+                </motion.div>
                 <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
                 <button>Contact Us</button>
             </Description>

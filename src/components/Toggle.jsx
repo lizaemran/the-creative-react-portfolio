804a782c35b0ea9cb718855038c7cd42bb166e09
lizaemran <lizaemran56@gmail.com>
2021-07-19 19:08:33 +0500

    15. Finishing FAQ Animation

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import {motion} from "framer-motion";
 const Toggle = ({children, title }) => {
-    const [toggle, setToggle] = useState(true);
+    const [toggle, setToggle] = useState(false);
     return (
         <motion.div Layout className="question" onClick = {()=> setToggle(!toggle)}>
             <motion.h4 Layout>{title}</motion.h4>

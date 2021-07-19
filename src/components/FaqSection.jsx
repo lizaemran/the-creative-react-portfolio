c02bbac244e118af419c77caa0a7c13612aafca3
lizaemran <lizaemran56@gmail.com>
2021-07-19 18:32:58 +0500

    15. toggle

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -1,40 +1,38 @@
 import React from 'react'
 import styled from 'styled-components';
 import {About} from '../styles';
+import Toggle from './Toggle';
 const FaqSection = () => {
-    return (
+   
+    return ( 
         <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How Do I Start?</h4>
                 <div className="answer">
                     <p>Lorem ipsum, dolor sit amet.</p>
-                    <p>Lorem ipsum, dolor sit amet Lorem ipsum, dolor sit amet</p>
-                </div>
+                    </div>
                 <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Daily Schedule</h4>
                 <div className="answer">
                     <p>Lorem ipsum, dolor sit amet.</p>
-                    <p>Lorem ipsum, dolor sit amet Lorem ipsum, dolor sit amet</p>
-                </div>
+                    </div>
                 <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Different Payment Methods</h4>
                 <div className="answer">
                     <p>Lorem ipsum, dolor sit amet.</p>
-                    <p>Lorem ipsum, dolor sit amet Lorem ipsum, dolor sit amet</p>
-                </div>
+                    </div>
                 <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What Products Do You Order?</h4>
                 <div className="answer">
                     <p>Lorem ipsum, dolor sit amet.</p>
-                    <p>Lorem ipsum, dolor sit amet Lorem ipsum, dolor sit amet</p>
-                </div>
+                    </div>
                 <div className="faq-line"></div>
             </div>
         </Faq>
